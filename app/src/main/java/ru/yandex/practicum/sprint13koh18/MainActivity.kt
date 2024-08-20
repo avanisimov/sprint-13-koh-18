54df747891322d3d4edddc83de1b13e0177ed493
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2024-08-20 19:28:54 +0300

    #1 Исправлено отображение заголовка при смене вкладки. Выбор ресурса названия режима экрана перенесён в енум

--- app/src/main/java/ru/yandex/practicum/sprint13koh18/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint13koh18/MainActivity.kt
@@ -185,15 +185,14 @@ class MainActivity : AppCompatActivity() {
                 ScreenMode.CATALOG -> {
                     binding.catalogContainer.visibility = View.VISIBLE
                     binding.cartContainer.visibility = View.GONE
-                    binding.toolbar.setTitle(R.string.catalog_title)
                 }
 
                 ScreenMode.CART -> {
                     binding.catalogContainer.visibility = View.GONE
                     binding.cartContainer.visibility = View.VISIBLE
-                    binding.toolbar.setTitle(R.string.cart_title)
                 }
             }
+            binding.toolbar.setTitle(newScreenMode.titleResId)
             currentScreenMode = newScreenMode
         }
     }
--- app/src/main/java/ru/yandex/practicum/sprint13koh18/ScreenMode.kt
+++ app/src/main/java/ru/yandex/practicum/sprint13koh18/ScreenMode.kt
@@ -3,3 +3,9 @@ package ru.yandex.practicum.sprint13koh18
 enum class ScreenMode {
     CATALOG, CART
 }
+
+val ScreenMode.titleResId: Int
+    get() = when (this) {
+        ScreenMode.CATALOG -> R.string.catalog_title
+        ScreenMode.CART -> R.string.cart_title
+    }
